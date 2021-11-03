5213a80094f65b840244bd6b27d9a01161a1b0aa
Kenneth Chen <kenchen0606@gmail.com>
2021-11-03 22:04:10 +0000

    wasd works

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,9 +16,19 @@ const setupInput = function () {
 
 const handleUserInput = function (key) {
   // your code here
+  if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  } 
   if (key === '\u0003') {
     process.exit();
   }
+  
 };
 
 module.exports = {setupInput};
--- play.js
+++ play.js
@@ -4,5 +4,5 @@ const { setupInput } = require("./input.js");
 
 // establishes a connection with the game server
 console.log("Connecting ...");
-connect();
-setupInput();
+let conn = connect();
+setupInput(conn);
